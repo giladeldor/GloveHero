8f0d4ee206d418b032891b9157c948c9076d1c39
Eran Cohen <ceranco@gmail.com>
2023-05-16 19:17:42 +0300

    Bigger fonts.

--- glove_hero_app/lib/utils.dart
+++ glove_hero_app/lib/utils.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 const titleTextStyle = TextStyle(
   fontFamily: "Karmatic",
-  fontSize: 40,
+  fontSize: 45,
   color: Color.fromARGB(255, 255, 240, 254),
 );
 
 const menuButtonTextStyle = TextStyle(
   fontFamily: "Karmatic",
-  fontSize: 25,
+  fontSize: 30,
   color: Color.fromARGB(255, 242, 255, 235),
 );
 
 const menuButtonDisabledTextStyle = TextStyle(
   fontFamily: "Karmatic",
-  fontSize: 25,
+  fontSize: 30,
   color: Colors.grey,
 );
