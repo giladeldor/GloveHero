a69457ff1dddac5b226d0568e3e48316fc8df452
Eran Cohen <ceranco@gmail.com>
2023-05-30 19:11:13 +0300

    Changed song.

--- glove_hero_app/lib/models/song.dart
+++ glove_hero_app/lib/models/song.dart
@@ -1,3 +1,182 @@
+class Song {
+  const Song({
+    required this.name,
+    required this.previewSpan,
+    required this.volume,
+    String? title,
+  }) : title = title ?? name;
+
+  final String name;
+  final String title;
+  final PreviewSpan previewSpan;
+  final double volume;
+
+  String get audioAsset => "assets/audio/$_assetTitle.mp3";
+  String get artAsset => "assets/song-art/$_assetTitle.jpg";
+
+  String get _assetTitle =>
+      name.replaceAll(".", "").split(' ').join('-').toLowerCase();
+}
+
+class PreviewSpan {
+  const PreviewSpan({required this.start, required this.end});
+
+  final int start;
+  final int end;
+  Duration get duration => Duration(seconds: end - start);
+}
+
+class SongManager {
+  static const List<Song> songs = [
+    Song(
+      name: "4 AM",
+      previewSpan: PreviewSpan(start: 0, end: 15),
+      volume: 0.5,
+    ),
+    Song(
+      name: "All My Life",
+      title: "All My\nLife",
+      previewSpan: PreviewSpan(start: 6, end: 17),
+      volume: 0.5,
+    ),
+    Song(
+      name: "Bad Guy",
+      previewSpan: PreviewSpan(start: 14, end: 27),
+      volume: 1.5,
+    ),
+    Song(
+      name: "Bohemian Rhapsody",
+      title: "Bohemian\nRhapsody",
+      previewSpan: PreviewSpan(start: 50, end: 70),
+      volume: 0.7,
+    ),
+    Song(
+      name: "Come as You Are",
+      title: "Come as\nYou Are",
+      previewSpan: PreviewSpan(start: 0, end: 17),
+      volume: 0.5,
+    ),
+    Song(
+      name: "Company Car",
+      previewSpan: PreviewSpan(start: 28, end: 44),
+      volume: 1,
+    ),
+    Song(
+      name: "Freed from Desire",
+      title: "Freed from\nDesire",
+      previewSpan: PreviewSpan(start: 58, end: 73),
+      volume: 0.7,
+    ),
+    Song(
+      name: "Good Riddance",
+      previewSpan: PreviewSpan(start: 0, end: 16),
+      volume: 0.5,
+    ),
+    Song(
+      name: "I Approached Ido",
+      title: "I Approached\nIdo",
+      previewSpan: PreviewSpan(start: 40, end: 60),
+      volume: 0.5,
+    ),
+    Song(
+      name: "In The End",
+      previewSpan: PreviewSpan(start: 0, end: 18),
+      volume: 0.4,
+    ),
+    Song(
+      name: "Karma Police",
+      previewSpan: PreviewSpan(start: 0, end: 27),
+      volume: 1.3,
+    ),
+    Song(
+      name: "Mario Theme",
+      previewSpan: PreviewSpan(start: 0, end: 17),
+      volume: 1.3,
+    ),
+    Song(
+      name: "Mr. Shoko",
+      previewSpan: PreviewSpan(start: 46, end: 58),
+      volume: 1.5,
+    ),
+    Song(
+      name: "My Love",
+      previewSpan: PreviewSpan(start: 0, end: 19),
+      volume: 0.4,
+    ),
+    Song(
+      name: "Nothing Else Matters",
+      title: "Nothing Else\nMatters",
+      previewSpan: PreviewSpan(start: 0, end: 27),
+      volume: 2,
+    ),
+    Song(
+      name: "Pokemon",
+      previewSpan: PreviewSpan(start: 0, end: 15),
+      volume: 2.5,
+    ),
+    Song(
+      name: "Seven Nation Army",
+      title: "Seven Nation\nArmy",
+      previewSpan: PreviewSpan(start: 48, end: 73),
+      volume: 1.7,
+    ),
+    Song(
+      name: "Slaves",
+      previewSpan: PreviewSpan(start: 19, end: 39),
+      volume: 0.7,
+    ),
+    Song(
+      name: "Stairway to Heaven",
+      title: "Stairway to\nHeaven",
+      previewSpan: PreviewSpan(start: 0, end: 25),
+      volume: 3.5,
+    ),
+    Song(
+      name: "Staying Alive",
+      previewSpan: PreviewSpan(start: 0, end: 19),
+      volume: 0.5,
+    ),
+    Song(
+      name: "Sultans Of Swing",
+      title: "Sultans Of\nSwing",
+      previewSpan: PreviewSpan(start: 143, end: 161),
+      volume: 1,
+    ),
+    Song(
+      name: "The Final Countdown",
+      title: "The Final\nCountdown",
+      previewSpan: PreviewSpan(start: 117, end: 136),
+      volume: 1,
+    ),
+    Song(
+      name: "Toxic",
+      previewSpan: PreviewSpan(start: 0, end: 14),
+      volume: 1,
+    ),
+    Song(
+      name: "Under the Bridge",
+      title: "Under the\nBridge",
+      previewSpan: PreviewSpan(start: 0, end: 28),
+      volume: 2,
+    ),
+    Song(
+      name: "Yo-Ya",
+      previewSpan: PreviewSpan(start: 25, end: 45),
+      volume: 1.5,
+    ),
+    Song(
+      name: "You Give Love A Bad Name",
+      title: "YGLABN",
+      previewSpan: PreviewSpan(start: 14, end: 60),
+      volume: 0.7,
+    ),
+  ];
+
+  static List<HighScore> getHighScores(songIndex) {
+    return List.empty();
+  }
+}
+
 class HighScore {
   String name = 'NO HIGH SCORE';
   int score = -1;
@@ -5,171 +184,5 @@ class HighScore {
   setHighScore(String name, int score) {
     this.name = name;
     this.score = score;
-  }
-}
-
-class Song {
-  List<int> touches = List<int>.empty();
-  int currentTouch = -1;
-  late String asset;
-  late String name;
-  late int clipStart;
-  late int clipEnd;
-  List<HighScore> highScores = [
-    HighScore(),
-    HighScore(),
-    HighScore(),
-    HighScore(),
-    HighScore(),
-    HighScore(),
-    HighScore(),
-    HighScore(),
-    HighScore(),
-    HighScore(),
-  ];
-
-  Song(
-      {required this.asset,
-      required this.name,
-      required this.clipStart,
-      required this.clipEnd});
-}
-
-class SongManager {
-  static List<Song> songs = [
-    Song(
-        name: "4 AM",
-        asset: "assets/audio/4-AM.mp3",
-        clipStart: 0,
-        clipEnd: 15),
-    Song(
-        name: "All My Life",
-        asset: "assets/audio/all-my-life.mp3",
-        clipStart: 6,
-        clipEnd: 17),
-    Song(
-        name: "Bad Guy",
-        asset: "assets/audio/bad-guy.mp3",
-        clipStart: 14,
-        clipEnd: 27),
-    Song(
-        name: "Bohemian Rhapsody",
-        asset: "assets/audio/bohemian-rhapsody.mp3",
-        clipStart: 50,
-        clipEnd: 70),
-    Song(
-        name: "Come as You Are",
-        asset: "assets/audio/come-as-you-are.mp3",
-        clipStart: 0,
-        clipEnd: 17),
-    Song(
-        name: "Company Car",
-        asset: "assets/audio/company-car.mp3",
-        clipStart: 28,
-        clipEnd: 44),
-    Song(
-        name: "Freed from Desire",
-        asset: "assets/audio/freed-from-desire.mp3",
-        clipStart: 58,
-        clipEnd: 73),
-    Song(
-        name: "Good Riddance",
-        asset: "assets/audio/good-riddance.mp3",
-        clipStart: 0,
-        clipEnd: 16),
-    Song(
-        name: "I Approached Ido",
-        asset: "assets/audio/yes-i-approached-ido.mp3",
-        clipStart: 40,
-        clipEnd: 60),
-    Song(
-        name: "In The End",
-        asset: "assets/audio/in-the-end.mp3",
-        clipStart: 0,
-        clipEnd: 18),
-    Song(
-        name: "Karma Police",
-        asset: "assets/audio/karma-police.mp3",
-        clipStart: 0,
-        clipEnd: 27),
-    Song(
-        name: "Mario Theme",
-        asset: "assets/audio/mario.mp3",
-        clipStart: 0,
-        clipEnd: 17),
-    Song(
-        name: "Mr. Shoko",
-        asset: "assets/audio/mr-shoko.mp3",
-        clipStart: 46,
-        clipEnd: 58),
-    Song(
-        name: "My Love",
-        asset: "assets/audio/my-love.mp3",
-        clipStart: 0,
-        clipEnd: 19),
-    Song(
-        name: "Nothing Else Matters",
-        asset: "assets/audio/nothing-else-matters.mp3",
-        clipStart: 0,
-        clipEnd: 27),
-    Song(
-        name: "Pokemon",
-        asset: "assets/audio/pokemon.mp3",
-        clipStart: 0,
-        clipEnd: 15),
-    Song(
-        name: "Seven Nation Army",
-        asset: "assets/audio/seven-nation-army.mp3",
-        clipStart: 48,
-        clipEnd: 73),
-    Song(
-        name: "Slaves",
-        asset: "assets/audio/slaves.mp3",
-        clipStart: 19,
-        clipEnd: 39),
-    Song(
-        name: "Stairway to Heaven",
-        asset: "assets/audio/stairway-to-heaven.mp3",
-        clipStart: 0,
-        clipEnd: 25),
-    Song(
-        name: "Staying Alive",
-        asset: "assets/audio/staying-alive.mp3",
-        clipStart: 0,
-        clipEnd: 19),
-    Song(
-        name: "Sultans Of Swing",
-        asset: "assets/audio/sultans-of-swing.mp3",
-        clipStart: 143,
-        clipEnd: 161),
-    Song(
-        name: "The Final Countdown",
-        asset: "assets/audio/the-final-countdown.mp3",
-        clipStart: 117,
-        clipEnd: 136),
-    Song(
-        name: "Toxic",
-        asset: "assets/audio/toxic.mp3",
-        clipStart: 0,
-        clipEnd: 14),
-    Song(
-        name: "Under the Bridge",
-        asset: "assets/audio/under-the-bridge.mp3",
-        clipStart: 0,
-        clipEnd: 28),
-    Song(
-        name: "Yo-Ya",
-        asset: "assets/audio/yo-ya.mp3",
-        clipStart: 25,
-        clipEnd: 45),
-    Song(
-        name: "YGLABN",
-        asset: "assets/audio/you-give-love-a-bad-name.mp3",
-        clipStart: 14,
-        clipEnd: 60),
-  ];
-
-  static List<HighScore> getHighScores(songIndex) {
-    return songs[songIndex].highScores;
   }
 }
