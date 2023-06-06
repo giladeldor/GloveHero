59b581a2e602f6e603dc7470bf71fd5d42aab5f2
nadavh22 <53226038+nadavh22@users.noreply.github.com>
2023-06-06 19:30:57 +0300

    Added Controls Page

--- glove_hero_app/lib/pages/controls_page.dart
+++ glove_hero_app/lib/pages/controls_page.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 
-class ControlsPage extends StatefulWidget {
+class ControlsPage extends StatelessWidget {
   const ControlsPage({super.key});
 
-  @override
-  State<ControlsPage> createState() => _ControlsPage();
-}
-
-class _ControlsPage extends State<ControlsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
+          color: Colors.black,
           image: DecorationImage(
-            image: AssetImage("assets/backgrounds/controls-glove.png"),
-            fit: BoxFit.cover,
+            image: AssetImage("assets/backgrounds/controls-glove.jpg"),
+            fit: BoxFit.fitWidth,
           ),
         ),
       ),
--- glove_hero_app/lib/pages/menu_page.dart
+++ glove_hero_app/lib/pages/menu_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:glove_hero_app/models/ble.dart';
+import 'package:glove_hero_app/pages/controls_page.dart';
 import 'package:glove_hero_app/pages/recording_mode_page.dart';
 import 'package:provider/provider.dart';
 import '../models/menu_action.dart';
@@ -71,7 +72,9 @@ class _MenuPageState extends State<MenuPage> {
         // Navigator.pushNamed(context, "/statistics");
         break;
       case _MenuButtonID.controls:
-        // Navigator.pushNamed(context, "/controls");
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const ControlsPage(),
+        ));
         break;
       default:
     }
