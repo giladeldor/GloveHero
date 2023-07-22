632fe32056e04466dd4fc5e80d43dfe64dd49294
Eran Cohen <ceranco@gmail.com>
2023-07-22 11:53:00 +0300

    Updated tests for new glove.

--- test/led_test.cpp
+++ test/led_test.cpp
@@ -1,7 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN 5
-#define NUM_PIXELS 12
+#define LED_PIN 26
+#define NUM_PIXELS 4
 #define BAUD_RATE 9600
 
 Adafruit_NeoPixel pixels(NUM_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -14,17 +14,14 @@ void setup() {
 }
 
 void loop() {
-
     pixels.clear();
     pixels.show();
 
-    for (int i = 0; i < 12; i++)
-    {
-        pixels.fill(pixels.Color(rand() % 255, rand() % 255, rand() % 255), i, 1);
-        
+    for (int i = 0; i < NUM_PIXELS; i++) {
+        pixels.setPixelColor(
+            i, pixels.Color(rand() % 255, rand() % 255, rand() % 255));
     }
-    
 
     pixels.show();
-    delay(300);
+    delay(500);
 }
