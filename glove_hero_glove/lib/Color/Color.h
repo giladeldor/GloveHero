e111d2c707888b5974bcf2820f6fc9f6cbd3cecc
Eran Cohen <ceranco@gmail.com>
2023-08-01 00:23:59 +0300

    Added docs for public API for ESP32 code.

--- glove_hero_glove/lib/Color/Color.h
+++ glove_hero_glove/lib/Color/Color.h
@@ -3,13 +3,16 @@
 
 #include <stdint.h>
 
+/// @brief A color POD.
 struct Color {
     uint8_t red;
     uint8_t green;
     uint8_t blue;
 
+    /// @brief Construct a new Color object.
     Color(uint8_t red, uint8_t green, uint8_t blue);
 
+    /// @brief Construct a new Color object from the data binary vector.
     static Color fromData(const char* data);
 };
 
--- glove_hero_glove/lib/Input/Input.h
+++ glove_hero_glove/lib/Input/Input.h
@@ -5,12 +5,25 @@
 
 #define NUM_INPUTS 4
 
-enum class Input { None = -1, Input1, Input2, Input3, Input4 };
+/// @brief The inputs on the glove.
+enum class Input {
+    None = -1,
+    Input1,
+    Input2,
+    Input3,
+    Input4,
+};
 
+/// @brief Converts an input to it's integer representation.
+/// @param input The input to convert.
+/// @return The integer representation of the input.
 static int toInt(Input input) {
     return static_cast<int>(input);
 }
 
+/// @brief Converts an input to it's string representation.
+/// @param input The input to convert.
+/// @return The string representation of the input.
 static String toString(Input input) {
     switch (input) {
         case Input::Input1:
@@ -26,20 +39,26 @@ static String toString(Input input) {
     }
 }
 
+/// @brief The possible states of an input.
 enum class InputState {
     Active,
     Inactive,
 };
 
+/// @brief Base class for input-manager classes.
 class InputBase {
 public:
     virtual ~InputBase() = default;
 
+    /// @brief Update the input state and return the current input.
+    /// @return The current input.
     virtual Input update() = 0;
 
+    /// @brief Get the input state of the given input.
     virtual InputState inputState(Input input) const = 0;
 };
 
+/// @brief A mock input manager for input given from keyboard over serial.
 class KeyboardInput final : public InputBase {
 public:
     KeyboardInput();
@@ -56,8 +75,14 @@ private:
     Input current;
 };
 
+/// @brief The input manager for the glove itself, using the touch pins.
 class GloveInput final : public InputBase {
 public:
+    /// @brief Construct a new GloveInput object.
+    /// @param pinky_pin The touch pin for the pinky.
+    /// @param ring_pin The touch pin for the ring finger.
+    /// @param middle_pin The touch pin for the middle finger.
+    /// @param index_pin The touch pin for the index finger.
     GloveInput(int pinky_pin, int ring_pin, int middle_pin, int index_pin);
 
     virtual Input update() override;
